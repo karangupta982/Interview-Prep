e1b2dc08542aa5ba903d15361d360d3131a238a7
Karan gupta <gupta.karan1.gh@gmail.com>
2025-12-01 00:37:33 +0530

    accordian arrows updated

--- machine-coding/app/components/accordion.jsx
+++ machine-coding/app/components/accordion.jsx
@@ -2,21 +2,21 @@
 
 import { useState } from "react";
 import data from "./data.js";
-import { ChevronDown } from 'lucide-react';
+import { ChevronDown,ChevronUp } from 'lucide-react';
 
 export default function Accordion(){
     const [show,setShow] = useState(null);
     return (
-        <div className="m-auto mt-10 text-white">
-            {
+        <div className="m-auto mt-10 text-white border-1 w-1/2 rounded-2xl p-2 pb-5">
+            { data.length > 0 ?
                 data.map((item) => {
-                    return <div className=" w-1/2 m-auto border-1 p-2" key={item.id}>
-                        <div className="flex justify-between">
+                    return <div className=" w-full m-auto  p-2" key={item.id}>
+                        <div className="flex justify-between border-b-2">
                             <h2>{item.question}</h2>
-                            <button> 
-                                <ChevronDown size={20} onClick={()=>{
-                                    setShow(show == item.id ? null : item.id);
-                                }}  className="cursor-pointer"/>
+                            <button className="cursor-pointer" onClick={()=>{
+                                setShow(show == item.id ? null : item.id);
+                            }}> 
+                                {show == item.id ? <ChevronUp size={20}/> : <ChevronDown size={20}/>}
                             </button>
                         </div>
                         {show == item.id && 
@@ -26,6 +26,8 @@ export default function Accordion(){
                         }
                     </div>
                 })
+                :
+                <h1>No accordian item present</h1>
             }
         </div>
     );
